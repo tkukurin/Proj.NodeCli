e1c85d91dbe2334ca022fcd73a0931bfc7d80fb7
Toni Kukurin <tkukurin@gmail.com>
2022-02-03 23:52:14 +0000

    Test utils and formatting very basic

---
+++ src/test/formatter.ts
@@ -0,0 +1,47 @@
+import {expect} from 'chai';
+import {AxiosResponse, AxiosError} from 'axios';
+import {DefaultFormatter, extractFirstEmail, extractTitleTag} from '@/formatter';
+
+describe('gets emails', () => {
+  it('no email', () => {
+    const s = 'I am a string without an email';
+    const email = extractFirstEmail(s);
+    expect(email).to.be.undefined;
+  });
+
+  it('basic extract first email', () => {
+    const s = 'I am a string me@email.com and another other@gmail.co.uk';
+    const email = extractFirstEmail(s);
+    expect(email).equal('me@email.com');
+  });
+
+  it('extract from href', () => {
+    const s = '<a href="mailto:x@gmail.com">x@gmail.com</a>';
+    const email = extractFirstEmail(s);
+    expect(email).equal('x@gmail.com');
+  });
+
+  it('does not get confused by @s', () => {
+    const s = 'I am a string me @email.com fail';
+    const email = extractFirstEmail(s);
+    expect(email).to.be.undefined;
+  });
+});
+
+describe('gets title tag', () => {
+  it('title', () => {
+    const s = 'tis is my <title>page title</title> and text around';
+    expect(extractTitleTag(s)).equal('page title');
+  });
+
+  it('title with id', () => {
+    const s = 'tis is my <title id="123">page title</title> and text around';
+    expect(extractTitleTag(s)).equal('page title');
+  });
+
+  it('title negative', () => {
+    const s = 'tis is my <titlenot>page title</title> and text around';
+    expect(extractTitleTag(s)).to.be.undefined;
+  });
+});
+
---
+++ src/test/utils.ts
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+import {uniq} from '@/utils';
+
+describe('uniq', async () => {
+  expect([1,1,2,3,1,1].filter(uniq())).to.have.all.members([1,2,3]);
+});
+
