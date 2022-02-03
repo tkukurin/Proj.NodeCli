b4f57e2896679c4643a5422528a54c1696ce7327
Toni Kukurin <tkukurin@gmail.com>
2022-02-03 23:35:19 +0000

    issue urlGet on urls

--- src/cli/tk-gotcha.ts
+++ src/cli/tk-gotcha.ts
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 import cli from 'commander';
 import parser from '@/parser';
-import {UniqFilter, urlNorm} from '@/utils';
+import {uniq, urlNorm} from '@/utils';
+import getter from '@/getter';
+
+const KEY_SHA_SALT = 'IM_SECRET'
+const SEC_TIMEOUT = 5;
+const SEC_BETWEEN_REQUESTS = 1;
+
+const shaSecret = process.env[KEY_SHA_SALT];
+if (!shaSecret) {
+  throw `Please set the '${KEY_SHA_SALT}' environment variable.`;
+}
 
 cli
   .description('tk-gotcha: toy app')
   .arguments('<user-input...>')
   .action((userinput: Array<string>) => {
-    const f = new UniqFilter();
-    const p = parser(userinput.join(''))
+    const urlGet = getter(SEC_TIMEOUT, SEC_BETWEEN_REQUESTS);
+    parser(userinput.join(''))
       .map(urlNorm)
-      .filter(x => f.seen(x));
-
-    console.log(p);
+      .filter(uniq())
+      .map(urlGet);
   }).parse();
 
